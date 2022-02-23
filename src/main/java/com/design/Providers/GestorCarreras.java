60b7c850abd5cf20900921c8aff815dbb0040339
JoseDanielRojas <43434891+JoseDanielRojas@users.noreply.github.com>
2022-02-23 08:32:09 -0600

    Diagram changes
    Diagram changes added, some import error appear.

--- src/main/java/com/design/Providers/GestorCarreras.java
+++ src/main/java/com/design/Providers/GestorCarreras.java
@@ -6,46 +6,52 @@ import com.design.Model.Carrera;
 import com.design.Model.Sede;
 import com.design.Model.TGrado;
 
-public class GestorCarreras{
+public class GestorCarreras {
 
-    public GestorCarreras(){
-        
+    public GestorCarreras() {
+
     }
-    
-    public boolean agregarCarreras(Carrera pCarrera){
+
+    public boolean agregarCarreras(Carrera pCarrera) {
         try {
-            
 
             return true;
         } catch (Exception e) {
-            //TODO: handle exception
+            // TODO: handle exception
             return false;
         }
     }
-    
-    public ArrayList<Carrera> buscarCarreras(){
+
+    public ArrayList<Carrera> buscarCarreras() {
         ArrayList<Carrera> resultado = new ArrayList<>();
         return resultado;
     }
-    
-    public Sede buscarSede(String pCodigo){
+
+    public Sede buscarSede(String pCodigo) {
         Sede sede = new Sede(pCodigo, "sede Encontrada");
         return sede;
     }
-    
-    public Carrera buscarCarrera(String pCodigo){
-        Carrera carrera = new Carrera(pCodigo, "Carrera encontrada", 546, 150, 
-                                        TGrado.BACHILLERATO, new Sede("AG132", "Una Sede"));
+
+    public Carrera buscarCarrera(String pCodigo) {
+        Carrera carrera = new Carrera(pCodigo, "Carrera encontrada", 546, 150,
+                TGrado.BACHILLERATO, new Sede("AG132", "Una Sede"));
         return carrera;
     }
-    
-    public boolean modificarCarrera(Carrera pCarrera){
+
+    public boolean modificarCarrera(Carrera pCarrera, String pCodigoSede) {
         try {
-            
+
             return true;
         } catch (Exception e) {
-            //TODO: handle exception
+            // TODO: handle exception
             return false;
         }
     }
+
+    public Carrera buscarCarreraPorSede(String pCodigoSede) {
+        Carrera carrera = new Carrera(pCodigoSede, "Carrera encontrada", 546, 150,
+                TGrado.BACHILLERATO, new Sede("AG132", "Una Sede"));
+        return carrera;
+    }
+
 }
--- src/main/java/com/design/Providers/GestorFormulario.java
+++ src/main/java/com/design/Providers/GestorFormulario.java
@@ -2,60 +2,71 @@ package com.design.Providers;
 
 import java.util.ArrayList;
 
+import com.design.Model.Carrera;
 import com.design.Model.Formulario;
+import com.design.Model.Sede;
 import com.design.Model.TEstado;
+import com.design.Model.TGrado;
 
 public class GestorFormulario {
-    
-    public GestorFormulario(){
-        
+
+    public GestorFormulario() {
+
     }
 
-    public boolean agregarFormulario(Formulario pFormulario){
+    public boolean agregarFormulario(Formulario pFormulario) {
         try {
-            
+
             return true;
         } catch (Exception e) {
-            //TODO: handle exception
+            // TODO: handle exception
             return false;
         }
     }
 
-    public boolean aplicarSimulacion(){
+    public boolean aplicarSimulacion() {
         try {
-            
+
             return true;
         } catch (Exception e) {
-            //TODO: handle exception
+            // TODO: handle exception
             return false;
         }
     }
 
-    public boolean definirResultados(){
+    public boolean definirResultados() {
         try {
-            
+
             return true;
         } catch (Exception e) {
-            //TODO: handle exception
+            // TODO: handle exception
             return false;
         }
     }
 
-    public ArrayList<Formulario> getSolicitudes(){
+    public ArrayList<Formulario> getSolicitudes() {
         ArrayList<Formulario> resultado = new ArrayList<>();
 
         return resultado;
     }
 
-    public ArrayList<Formulario> getSolicitudesPorNombre(TEstado pEstado){
+    public ArrayList<Formulario> getSolicitudesPorNombre(TEstado pEstado) {
         ArrayList<Formulario> resultado = new ArrayList<>();
 
         return resultado;
     }
 
-    public ArrayList<Formulario> getSolicitudesPorPuntaje(TEstado pEstado){
+    public ArrayList<Formulario> getSolicitudesPorPuntaje(TEstado pEstado) {
         ArrayList<Formulario> resultado = new ArrayList<>();
 
         return resultado;
-    }    
+    }
+
+    public Formulario buscarFormulario(String pCodigo) {
+        Formulario formulario = new Formulario(1, TEstado.ACEPTADO, new Carrera("1",
+                "Carrera encontrada", 546, 150, TGrado.BACHILLERATO,
+                new Sede("AG132", "Una Sede")), 1);
+        return formulario;
+    }
+
 }
